530d4fcab4873902869e3401ee83db5f5c5535d5
ai-technipreneurs <83169416+ai-technipreneurs@users.noreply.github.com>
2021-07-09 16:19:06 +0200

    Update app.py

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,5 +1,27 @@
-from flask import Flask
+ffrom flask import Flask
+from flask import json
+
 app = Flask(__name__)
+
+@app.route('/status')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    return response
 
 @app.route("/")
 def hello():
@@ -7,3 +29,4 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
